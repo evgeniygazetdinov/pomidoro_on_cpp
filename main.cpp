97dd7c63af4196190c4f5aad97e69692d483e071
Gazetdinov <yngazetd@mts.ru>
2023-04-14 16:48:06 +0300

    first implement timer

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+using namespace std;
+
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
+
+
+
+
+void displayClock() {
+    cout <<setfill(' ') <<setw(55) <<"         TIMER         \n";
+    cout <<setfill(' ') <<setw(55) <<" --------------------------\n";
+    cout <<setfill(' ') <<setw(29);
+
+    cout <<"| " <<setfill('0') <<setw(2) <<hours <<" hrs | ";
+    cout <<setfill('0') <<setw(2) <<minutes <<" min | ";
+    cout <<setfill('0') <<setw(2) <<seconds <<" sec |" <<endl;
+    cout <<setfill(' ') <<setw(55) <<" --------------------------\n";
+}
+
+void timer() {
+
+    while (true) {
+        system("clear");
+
+        displayClock();
+        sleep(1);
+        seconds++;
+
+
+        if (seconds == 60) {
+
+            minutes++;
+
+            if (minutes == 60) {
+                hours++;
+                minutes = 0;
+            }
+
+            seconds = 0;
+        }
+    }
+}
+
+// add handle space for stop timer in thread!!!!
+
+int main() {
+    timer();
+    return 0;
+}
