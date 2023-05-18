0fade5125d91f89b156cbd49c96fa3e3a8751434
Gazetdinov <yngazetd@mts.ru>
2023-05-18 06:31:51 +0300

    prepare to move to sfml

--- main.cpp
+++ main.cpp
@@ -4,47 +4,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string>
-#include <conio.h>
-#define KEY_UP    72
-#define KEY_LEFT  75
-#define KEY_RIGHT 77
-#define KEY_DOWN  80
-
-
+#include <SFML/Graphics.hpp>
 
 
 using namespace std;
 
-
-void input()
-{
-    int c = 0;
-    switch ((c = getch()))
-    {
-    case KEY_UP:
-        cout << endl<< "Up" << endl; //key up
-        break;
-    case KEY_DOWN:
-        cout << endl<< "Down" << endl; // key down
-        break;
-    case KEY_LEFT:
-        cout << endl<< "Left" << endl; // key left
-        break;
-    case KEY_RIGHT:
-        cout << endl<< "Right" << endl; // key right
-        break;
-    default:
-        break;
-    }
-}
-
-
 int hours = 0;
 int minutes = 0;
 int seconds = 0;
-
-
-
 
 void displayClock() {
     cout <<setfill(' ') <<setw(55) <<"         TIMER         \n";
@@ -61,11 +28,9 @@ void timer() {
 
     while (true) {
         system("clear");
-        string pressed_key;
         displayClock();
         sleep(1);
         seconds++;
-
         
         if (seconds == 60) {
 
---
+++ make.sh
@@ -0,0 +1,12 @@
+
+FILE=pomidoro;
+COMPILED_FILE="main.cpp";
+COMPILE_TEXT="g++ "$COMPILED_FILE" -I/usr/local/Cellar/sfml/2.5.1.2/includ/e -o "$FILE" -L /usr/local/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system";
+if [ -f "$FILE" ]; then
+    eval "rm -rf $FILE";
+    eval $COMPILE_TEXT;                 
+    eval "./$FILE";
+else 
+    eval $COMPILE_TEXT;
+    eval "./$FILE";
+fi
