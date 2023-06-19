e6556ba3fc713758bf9d84c1c51dd7732ed4ce90
Gazetdinov <yngazetd@mts.ru>
2023-06-19 09:06:27 +0300

    in proccess mouse handling

--- proto.cpp
+++ proto.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 
 int hours, seconds, minutes;
+const int SCRWIDTH = 1280;
+const int SCRHEIGHT = 720;
 
 int main()
 {
@@ -28,6 +30,16 @@ int main()
     timeWidget.setPosition(400,200);
     TextTimer timer;
 
+    sf::Texture background;
+    sf::Sprite backgroundImage;
+
+    if (!background.loadFromFile("sprites/startButton.png"))
+        std::cout << "Error: Could not display Alaskan Wilderness image" << std::endl;
+
+    backgroundImage.setTexture(background);
+    backgroundImage.setPosition(sf::Vector2f(SCRWIDTH/2.0f,SCRHEIGHT/2.0f));
+
+
     while (window.isOpen())
     {
         dt = clock.getElapsedTime().asSeconds();
@@ -39,10 +51,27 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonPressed)
+{
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    std::cout << "the right button was pressed" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                    if(event.mouseButton.x > 600 && event.mouseButton.x < 884 || event.mouseButton.y >482 && event.mouseButton.y <510 ){
+                                         std::cout << "button presed" std::endl;
+   
+                    // TODO HANDLE push without handcode 
+                    }
+                }
+        }
         }
         timeWidget.setString(timer.prepareTimeString(lastTime));
         window.clear();
+                
+
         window.draw(timeWidget);
+        window.draw(backgroundImage);
         window.display();
 
     }
